cb973841f28fdbeaf01970e732fff40a333faa0b
sirsegv <sirsegv@sirsegv.moe>
2026-07-19 22:39:29 +0930

    Add escaped json test

--- _theme_bundle_test.v
+++ _theme_bundle_test.v
@@ -290,3 +290,14 @@ fn test_theme_load_dir() {
 	}
 	assert b.name == 'light'
 }
+
+fn test_theme_parse_escaped_json() {
+	content := '{
+		"name": "esc\u0061ped"
+	}'
+	t := theme_parse(content) or {
+		assert false, err.str()
+		return
+	}
+	assert t.name == 'escaped'
+}
